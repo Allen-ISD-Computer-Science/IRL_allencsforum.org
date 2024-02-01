c707935c367abb00bdf0f0a61b4d43720d9ab535
Lettuce287 <sarch4ms@gmail.com>
2024-02-01 13:09:24 -0600

    added www folder

---
+++ www/src/index.js
@@ -0,0 +1,14 @@
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App.js';
+
+export * from './components/sfconstants.js';
+
+const root = createRoot(document.getElementById('app'));
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
