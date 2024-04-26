cba6e67d7432834a165349a862c80454eda74e6b
Lettuce287 <sarch4ms@gmail.com>
2024-04-26 13:05:34 -0500

    starting clubs website, new file with placeholder constants to be mapped & grid-ified later

---
+++ dev_build/src/components/clubsconstants.js
@@ -0,0 +1,47 @@
+export const clubsListed = [
+    {
+	name: 'Computer Science Honor Society',
+	previewimg: '../assets/lou.jpg',
+	longdesc: 'i hate it here'
+    },
+    {
+	name: 'Cybersecurity Club',
+	previewimg: '../assets/lou.jpg',
+	longdesc: 'this club also stinks'
+    },
+    {
+	name: 'OC3',
+	previewimg: '../assets/lou.jpg',
+	longdesc: 'yep. this is a description'
+    },
+    {
+	name: 'Project RENEW',
+	previewimg: '../assets/lou.jpg',
+	longdesc: 'wow. this project is DEAD.'
+    },
+    {
+	name: 'Mr. Ben Merch Store',
+	previewimg: '../assets/lou.jpg',
+	longdesc: 'really dont know what this club does'
+    },
+    {
+	name: 'Project MAE',
+	previewimg: '../assets/lou.jpg',
+	longdesc: 'do people actually work on this'
+    },
+    {
+	name: 'IRL Dev / Internships',
+	previewimg: '../assets/lou.jpg',
+	longdesc: 'have not seen anything come out of this club either'
+    },
+    {
+	name: 'Game Development',
+	previewimg: '../assets/lou.jpg',
+	longdesc: 'do people actually participate in this club?'
+    },
+    {
+	name: 'Girls Who Code',
+	previewimg: '../assets/lou.jpg',
+	longdesc: 'pretty sure this club is defunct but i need 9 tiles'
+    }
+]
